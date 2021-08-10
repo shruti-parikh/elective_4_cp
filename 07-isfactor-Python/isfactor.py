1fa0680f8f60821b5f0ed774fe5bc1ef8da04cdd
Shruti <Shrutiparikh40@msitprogram.net>
2021-08-10 23:35:37 +0530

    all testcases passed

--- 07-isfactor-Python/isfactor.py
+++ 07-isfactor-Python/isfactor.py
@@ -6,4 +6,12 @@
 
 
 def fun_isfactor(f, n):
-	return False # replace with your solution
+	if f == 0 and n != 0:
+		return False
+	elif  n == 0:
+		return True
+	elif n%f == 0:
+		return True
+	else:
+		return False
+	
