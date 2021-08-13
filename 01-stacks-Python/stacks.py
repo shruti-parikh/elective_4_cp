ffddeaeca46a8430eb337e53767a3c2f85960f7b
Shruti <Shrutiparikh40@msitprogram.net>
2021-08-13 10:43:25 +0530

    all testcases passed

--- 01-stacks-Python/stacks.py
+++ 01-stacks-Python/stacks.py
@@ -26,11 +26,19 @@ class LinkedList(object):
 
     def insert_first(self, new_element):
         "Insert new element as the head of the LinkedList"
-        pass
+        new_element.next = self.head
+        self.head = new_element
+
+        
 
     def delete_first(self):
         "Delete the first (head) element in the LinkedList as return it"
-        pass
+        del_first = self.head
+        if self.head:
+            self.head = del_first.next
+            del_first.next = None
+        return del_first
+
 
 class stack(object):
     def __init__(self,top=None):
@@ -38,9 +46,9 @@ class stack(object):
 
     def push(self, new_element):
         "Push (add) a new element onto the top of the stack"
-        pass
+        self.ll.insert_first(new_element)
 
     def pop(self):
         "Pop (remove) the first element off the top of the stack and return it"
-        pass
-    
+        return self.ll.delete_first()
+        
