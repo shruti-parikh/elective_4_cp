c94ffa14679e753d4e20b6ebd6700150468d224c
Shruti <Shrutiparikh40@msitprogram.net>
2021-08-13 15:51:17 +0530

    all testcases passed

--- 03-quicksort-Python/quicksort.py
+++ 03-quicksort-Python/quicksort.py
@@ -1,6 +1,26 @@
 """Implement quick sort in Python.
 Input a list.
 Output a sorted list."""
+
+def partition(array, low, high):
+	pivot = array[high]
+	i = low - 1
+	for j in range(low, high):
+		if array[j] <= pivot:
+			i = i + 1
+			(array[i], array[j]) = (array[j], array[i])
+	(array[i + 1], array[high]) = (array[high], array[i + 1])
+
+	return i + 1
+
+def quick_sort(array, low, high):
+	if low < high:
+		pi = partition(array, low, high)
+		quick_sort(array, low, pi - 1)
+		quick_sort(array, pi + 1, high)
+ 
 def quicksort(array):
 	# Your code goes here
-	pass
+	quick_sort(array, 0, len(array) - 1)
+	print(array)
+	return array
