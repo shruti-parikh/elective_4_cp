6bb4b99415d26d08b6f9e285311867a021de77bf
Shruti <Shrutiparikh40@msitprogram.net>
2021-08-13 11:37:58 +0530

    all testcases passed

--- 15-isrighttriangle-Python/isrighttriangle.py
+++ 15-isrighttriangle-Python/isrighttriangle.py
@@ -5,6 +5,20 @@
 # distance(x1, y1, x2, y2), which you might call several times. Also, remember to use
 # almostEqual (instead of ==) when comparing floats.
 
+
+
+import math
 def isrighttriangle(x1, y1, x2, y2, x3, y3):
-	# your code goes here
-	pass
+	a = ((((x2-x1)**2)+((y2-y1)**2))**0.5)
+	b = ((((x3-x2)**2)+((y3-y2)**2))**0.5)
+	c = ((((x3-x1)**2)+((y3-y1)**2))**0.5)
+	
+	if round((a**2)+(b**2))==round(c**2):
+		return True
+	elif round((a**2)+(c**2))==round(b**2):
+		return True
+	elif round((b**2)+(c**2))==round(a**2):
+		return True
+	else:
+		return False
+
