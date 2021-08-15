e38f78e90b04f82be99252e93b3681eb0c93465a
Shruti <Shrutiparikh40@msitprogram.net>
2021-08-15 16:14:13 +0530

    all testcases passed

--- 01-matrixmultiply-Python/matrixmultiply.py
+++ 01-matrixmultiply-Python/matrixmultiply.py
@@ -5,7 +5,19 @@
 
 
 def fun_matrixmultiply(m1, m2):
-    return None
+    m1_row = len(m1)
+    m1_col = len(m1[0])
+    m2_row = len(m2)
+    m2_col = len(m2[0])
+    if m1_col != m2_row:
+        return None
+    mat_mul = [[0 for x in range (m2_col)] for y in range(m1_row)]
+    for i in range(m1_row):
+        for j in range(m2_col):
+            for k in range(m2_row):
+                mat_mul[i][j] += m1[i][k] * m2[k][j]
+
+    return mat_mul
 
 
 
