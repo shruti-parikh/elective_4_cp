8e6ed0e7eab86959541551f141cbe3043dd18283
Shruti <Shrutiparikh40@msitprogram.net>
2021-08-15 16:34:11 +0530

    testcases passed

--- multiply_polynomials.py
+++ multiply_polynomials.py
@@ -3,12 +3,19 @@
 # Write the function multiplyPolynomials(p1, p2) which takes two polynomials 
 # and returns a third polynomial which is the product of the two. For example,
 # multiplyPolynomials([2,0,3], [4,5]) represents the problem (2x**2 + 3)(4x + 5), and:
-# (2x**22 + 3)(4x + 5) = 8x**3 + 10x**2 + 12x + 15
+# (2x**2 + 3)(4x + 5) = 8x**3 + 10x**2 + 12x + 15
 # And so this returns [8, 10, 12, 15].
 
 def multiplyPolynomials(p1, p2):
     # Your code goes here...
-    pass
+    m = len(p1)
+    n = len(p2)
+    prod = [0] * (m + n - 1)
+    for i in range(m):
+        for j in range(n):
+            prod[i + j] += p1[i] * p2[j]
+    return prod
 
 # Write your own test cases
+print(multiplyPolynomials([2,0,3], [4,5]))
 print ("All test cases passwed...")
