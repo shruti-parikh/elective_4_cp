871fc5d9e878d81cafd6ee80d8618fe04f69f408
Shruti <Shrutiparikh40@msitprogram.net>
2021-08-29 01:46:41 +0530

    all testcases passed

--- LatinSquare.py
+++ LatinSquare.py
@@ -9,4 +9,28 @@
 
 def isLatinSquare(lst):
     # Your code goes here...
-    pass
+    n = len(lst)
+    rows = []
+    row = 0
+    col = 0
+    v = 0
+    for i in range(n):
+        rows.append(set([]))
+    cols = []
+    for i in range(n):
+        cols.append(set([]))
+    for i in range(n):
+        for j in range(n):
+            rows[i].add(lst[i][j])
+            cols[j].add(lst[i][j])
+            if (lst[i][j] > n or lst[i][j] <= 0):
+                v = v + 1
+    for i in range(n):
+        if (len(rows[i]) != n):
+            row = row + 1
+        if (len(cols[i]) != n):
+            col = col + 1
+    if (col == 0 and row == 0 and v == 0):
+        return True
+    else:
+        return False
