7721600ff34fccf82edd4937022edab725e58986
Shruti <Shrutiparikh40@msitprogram.net>
2021-08-31 20:36:19 +0530

    all testcases passed

--- 09-shortenlongruns-Python/shortenlongruns.py
+++ 09-shortenlongruns-Python/shortenlongruns.py
@@ -1,3 +1,4 @@
+
 # shortenLongRuns(L, k) [15 pts]
 # Write the non-destructive function shortenLongRuns(L, k) that takes a list L and a positive integer k, and 
 # non-destructively returns a similar list, only without any run of k consecutive equal values in L. This means that 
@@ -8,7 +9,20 @@
 # Note: your function may not just create a copy of L and call the destructive version of this function (below) on 
 # that copy and return it. Instead, you must directly construct the result here.
 
-
 def shortenlongruns(L, k):
 	# Your code goes here
-	pass
+	l = len(L)
+	count = 0
+	lst = []
+	val = k-1
+	for i in range(l-val):
+		if L[i] == L[i + 1] and L[i + 1] == L[i + 2]:
+			ele = L[i]
+	for j in L:
+		if j == ele:
+			count = count + 1
+		if count >= k and j == ele:
+			continue
+		else:
+			lst.append(j)
+	return lst
