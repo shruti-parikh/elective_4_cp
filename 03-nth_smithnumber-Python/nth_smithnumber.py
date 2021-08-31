f9bf23a643d771fa366e18d4637161fa49738ec3
Shruti <Shrutiparikh40@msitprogram.net>
2021-08-31 22:37:47 +0530

    all testcases passed

--- 03-nth_smithnumber-Python/nth_smithnumber.py
+++ 03-nth_smithnumber-Python/nth_smithnumber.py
@@ -8,5 +8,49 @@
 # so fun_nthsmithnumber(1) should return 22
 
 
+def prime(n):
+    if(n < 2):
+        return False
+    for i in range(2,n):
+        if(n % i == 0):
+            return False
+    return True
+
+def sumdigits(n):
+    s = 0
+    while(n > 0):
+        s = s + (n % 10)
+        n = n // 10
+    return s
+def primefact(n):
+    i = 2
+    s = 0
+    l = []
+    while(i*i <= n):
+        if(n % i == 0):
+            s = s + sumdigits(i)
+            l.append(i)
+            n = n // i
+        else:
+            i = i + 1
+    if(n > 1):
+        l.append(n)
+        s = s + sumdigits(n)
+    return s
+
+def smith(n):
+    if(prime(n)):
+        return False
+    if(sumdigits(n) == primefact(n)):
+        return True
+    return False
+
+
 def fun_nth_smithnumber(n):
-    return 1
+    c = 0
+    i = 0
+    while(c <= n):
+        i = i+ 1
+        if(smith(i)):
+            c = c+ 1
+    return i
